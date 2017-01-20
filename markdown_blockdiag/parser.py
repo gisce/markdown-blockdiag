631a1e4c70920d3464516f930004eb71bd0857de
Eduard Carreras <ecarreras@gisce.net>
2017-01-20 14:28:30 +0100

    Refactoring

--- markdown_blockdiag/parser.py
+++ markdown_blockdiag/parser.py
@@ -4,9 +4,7 @@ import os
 
 from markdown.blockprocessors import BlockProcessor
 from markdown.util import etree
-from markdown_blockdiag.utils import random_filename
-
-from blockdiag import parser, builder, drawer
+from markdown_blockdiag.utils import draw_blockdiag, random_filename
 
 
 class BlockdiagProcessor(BlockProcessor):
@@ -35,17 +33,16 @@ class BlockdiagProcessor(BlockProcessor):
         if not os.path.exists(diagram_dir):
             os.makedirs(diagram_dir)
 
-        raw_block = blocks.pop(0)
-        tree = parser.parse_string(raw_block)
-        diagram = builder.ScreenNodeBuilder.build(tree)
         filename = random_filename()
+
         draw_path = os.path.join(
             diagram_dir,
             filename
         )
-        draw = drawer.DiagramDraw('png', diagram, filename=draw_path)
-        draw.draw()
-        draw.save()
+
+        raw_block = blocks.pop(0)
+        diagram = draw_blockdiag(raw_block, draw_path)
+
         p = etree.SubElement(parent, 'p')
         img = etree.SubElement(p, 'img')
         img.attrib['src'] = '/diagrams/{}'.format(filename)
