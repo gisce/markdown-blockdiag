86b703b42e55a1139fd305503ebf6e66010b54c3
Manuel Barkhau <mb@nexttuesday.de>
2017-03-06 12:09:51 +0100

    update tests

--- tests/test_blockdiag.py
+++ tests/test_blockdiag.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import unittest
 import base64
+from xml.sax.saxutils import unescape
 
 from markdown import markdown
 from markdown_blockdiag.parser import BlockdiagProcessor
@@ -13,33 +14,44 @@ def join_text(lines):
     return '\n'.join(lines)
 
 
+TEST_DIAG_TXT = join_text([
+    "blockdiag {",
+    "    A -> B -> C;",
+    "}"
+])
+
+
 class BlockdiagTest(unittest.TestCase):
     """Testing blockdiag extension
     """
 
     def test_run(self):
-        text = join_text([
-            "blockdiag {",
-            "    A -> B -> C;",
-            "}"
-        ])
-        self.assertTrue(BlockdiagProcessor.RE.match(text))
+        self.assertTrue(BlockdiagProcessor.RE.match(TEST_DIAG_TXT))
 
     def test_basic_blockdiag(self):
-        text = join_text([
-            "blockdiag {",
-            "    A -> B -> C;",
-            "}"
-        ])
-
-        draw = draw_blockdiag(text)
+        draw = draw_blockdiag(TEST_DIAG_TXT)
 
         expected = '<p><img src="data:image/png;base64,{0}" /></p>'.format(
             base64.b64encode(draw)
         )
         result = markdown(
-            text,
-            extensions=['markdown_blockdiag']
+            TEST_DIAG_TXT,
+            extensions=['markdown_blockdiag'],
+            # extension_configs={'markdown_blockdiag': {'format': 'png'}}
         )
+
+        self.assertEqual(expected, result)
+
+    def test_svg_blockdiag(self):
+        self.maxDiff = None
+        draw = draw_blockdiag(TEST_DIAG_TXT, output_fmt='svg')
+
+        expected = '<p><img src="data:image/svg+xml;utf8,{0}" /></p>'.format(draw)
+        result = markdown(
+            TEST_DIAG_TXT,
+            extensions=['markdown_blockdiag'],
+            extension_configs={'markdown_blockdiag': {'format': 'svg'}}
+        )
+        result = unescape(result).replace('&quot;', '\"')
 
         self.assertEqual(expected, result)
