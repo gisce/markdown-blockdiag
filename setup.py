6369d63f713c09be0ea0e75ee818abe5ae0bdd92
Eduard Carreras <ecarreras@gisce.net>
2017-03-16 18:00:58 +0100

    Bump to v0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='markdown-blockdiag',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     url='https://github.com/gisce/markdown-blockdiag',
     license='MIT',
